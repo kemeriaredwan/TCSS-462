79ca110a66c43b94e1e24eace8a41bbaacdea10c
Bettyabera <bettyabe@uw.edu>
2022-11-27 16:21:45 -0500

    Transform

---
+++ java_template/src/main/java/lambda/Transform.java
@@ -0,0 +1,18 @@
+
+package lambda;
+
+import java.io.IOException;
+import java.util.HashMap;
+import jdk.javadoc.internal.doclets.toolkit.builders.AbstractBuilder.Context;
+
+/**
+ *
+ * @author betelhem
+ */
+public class Transform {
+    public HashMap<String,Object> handleRequest(Request request, Context context) throws IOException{
+        
+        return null;
+        
+    }
+}
