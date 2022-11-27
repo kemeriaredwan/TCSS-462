5fe2c8f211b965ec8eea434161189cb465ca516e
Bettyabera <bettyabe@uw.edu>
2022-11-27 17:34:47 -0500

    load/Trans

---
+++ TCSS-462/java_template/src/main/java/lambda/Transform.java
@@ -0,0 +1,19 @@
+
+package lambda;
+
+import java.io.IOException;
+import java.util.HashMap;
+import jdk.javadoc.internal.doclets.toolkit.builders.AbstractBuilder;
+
+/**
+ *
+ * @author betelhem
+ */
+public class Transform {
+      public HashMap<String, Object> handleRequest(Request request, AbstractBuilder.Context context) throws IOException{
+        
+    
+      return null;
+      }
+    
+}
